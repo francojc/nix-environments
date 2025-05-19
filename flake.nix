8f54b1c7b7f7df5a692475f788a08c6cbfd1b757
Jerid Francom <francojc@wfu.edu>
2025-05-18 22:28:12 -0400

    chore: Update template and add .envrc and .gitignore

--- flake.nix
+++ flake.nix
@@ -10,17 +10,16 @@
     self,
     nixpkgs,
     flake-utils,
-  } @ inputs:
-    let
-      # Define the available templates
-      templates = {
-        default = {
-          description = "An R programming environment template";
-          path = ./templates/default;
-        };
-        # Add other templates here as they are created
+  } @ inputs: let
+    # Define the available templates
+    templates = {
+      default = {
+        description = "An R programming environment template";
+        path = ./templates/default;
       };
-    in
+      # Add other templates here as they are created
+    };
+  in
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
     in {
@@ -37,8 +36,9 @@
           echo "Template management environment activated"
           echo "Available templates:"
           echo "- default: An R programming environment template"
-          echo "To create a new project from a template, navigate to the desired directory and run 'nix flake init -t github:your-username/your-repo#template-name'"
+          echo "To create a new project from a template, navigate to the desired directory and run 'nix flake init -t github:francojc/nix-environments#template-name'"
         '';
       };
-    }) // { inherit templates; }; # Export templates at the top level
+    })
+    // {inherit templates;}; # Export templates at the top level
 }
