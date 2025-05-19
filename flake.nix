5badd4558c66fc947b5daf9f059cdff19d63e5bb
Jerid Francom <francojc@wfu.edu>
2025-05-18 22:16:46 -0400

    refactor: Export templates at the top level of flake.nix

--- flake.nix
+++ flake.nix
@@ -10,10 +10,8 @@
     self,
     nixpkgs,
     flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in {
+  } @ inputs:
+    let
       # Define the available templates
       templates = {
         default = {
@@ -22,7 +20,10 @@
         };
         # Add other templates here as they are created
       };
-
+    in
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
       # Define a default development shell for the root of the repository
       devShell = pkgs.mkShell {
         packages = with pkgs; [
@@ -39,5 +40,5 @@
           echo "To create a new project from a template, navigate to the desired directory and run 'nix flake init -t github:your-username/your-repo#template-name'"
         '';
       };
-    });
+    }) // { inherit templates; }; # Export templates at the top level
 }
